7eeedb2821af8f5dd26cebb1ed052dd77591b788
SajjanKarn <49333264+SajjanKarn@users.noreply.github.com>
2020-09-13 22:37:07 +0545

    Added Homepage component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Homepage from './homepage.component';
+
+class App extends Component {
+  render() {
+    return <div className='App'>
+      <Homepage />
+    </div>;
+  }
+}
+
+export default App;
