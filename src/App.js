5281f62e64a7022315883dc888e59ab945460902
SajjanKarn <49333264+SajjanKarn@users.noreply.github.com>
2020-09-14 10:37:01 +0545

    updating collection-item collection-preview shopPage and giving them styles

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 import "./App.css";
 
+import ShopPage from "./pages/shoppage/shoppage.component";
+
 class App extends Component {
   render() {
-    const HatsPage = () => (
-      <div className="hatsPage">
-        <h1>HATS Page</h1>
-      </div>
-    );
     return (
       <div className="App">
         <Switch>
           <Route path="/" exact component={Homepage} />
-          <Route path="/hats" exact component={HatsPage} />
+          <Route path="/shop" exact component={ShopPage} />
         </Switch>
       </div>
     );
