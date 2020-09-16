3e4411490eb04c8738b4776cb2a5320fc7674ddd
SajjanKarn <49333264+SajjanKarn@users.noreply.github.com>
2020-09-16 22:37:40 +0545

    updating reselct to our cartitems, cart-item-counts, upading cart-dropdown component, cart-icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,18 +4,23 @@ import { connect } from "react-redux";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg.svg";
 
 import { toggleHiddenCart } from "../../redux/cart/cart.actions";
+import { selectCartItemCount } from "../../redux/cart/cart.selectors";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleHiddenCart }) => (
+const CartIcon = ({ toggleHiddenCart, itemCount }) => (
   <div className="cart-icon" onClick={toggleHiddenCart}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemCount(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleHiddenCart: () => dispatch(toggleHiddenCart()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
